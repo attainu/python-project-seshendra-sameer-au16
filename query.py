626d4816e9a62a7369121a4e97fbcccd9493a3ed
seshendra-sameer-au16 <sameer22051998@gmail.com>
2021-02-16 20:13:37 +0530

    added function to query the registration number of cars with a given color

--- query.py
+++ query.py
@@ -9,3 +9,17 @@ def status(currently_parked):
     headers = ['slot', 'reg_no', 'color']
 
     print(tabulate(vals, headers))
+
+
+def registration_numbers_for_cars_with_colour(color, cars_arrived):
+    temp = []
+    for i in cars_arrived:
+        if i[1] == color:
+            temp.append(i[0])
+
+    if len(temp) == 0:
+        print("No %s car has entered this parking lot" % color)
+        return
+    for i in temp[:len(temp) - 1]:
+        print(i, end=", ")
+    print(temp[-1])
