baa94abcb5ff6045a8a3ea85870855dc61bcdd87
seshendra-sameer-au16 <sameer22051998@gmail.com>
2021-02-16 18:56:10 +0530

    imported create_parking_lot function and applied it

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import os
+from create_parking_lot import create_parking_lot
+
+
+class Solution:
+    def __init__(self):
+        self.cars_arrived = []
+        self.cars_parked = {}
+        self.currently_parked = {}
+
+    def solve(self, cmd):
+        if cmd[0] == "create_parking_lot":
+            if len(cmd) == 2:
+                self.currently_parked = create_parking_lot(
+                    int(cmd[1]), self.currently_parked)
+            else:
+                print("Invalid command")
+                return
 
 
 if __name__ == "__main__":
+    s = Solution()
     while True:
         cmd = input("$ ")
         if len(cmd) == 0:
@@ -13,12 +31,12 @@ if __name__ == "__main__":
                 with open(cmd) as textfile:
                     for line in textfile:
                         line = line.rstrip("\n")
-                        solve(line.strip().split())
+                        s.solve(line.strip().split())
                 break
             except:
                 continue
         else:
             try:
-                solve(cmd.strip().split())
+                s.solve(cmd.strip().split())
             except:
                 continue
