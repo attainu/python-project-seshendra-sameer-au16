dd550b714e2fe7ca8effbf5ef23d478717b76a32
seshendra-sameer-au16 <sameer22051998@gmail.com>
2021-02-17 09:13:10 +0530

    made minor changes to get a clean code

--- query.py
+++ query.py
@@ -6,6 +6,7 @@ def status(currently_parked):
     for key, value in currently_parked.items():
         if value is not False:
             vals.append([key, value[0], value[1]])
+
     headers = ['slot', 'registration_no', 'color']
 
     print(tabulate(vals, headers))
